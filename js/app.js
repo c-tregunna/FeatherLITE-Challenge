844de30d6d7ee105065fb16fa1e342393a85d3b7
Claire Tregunna <c.tregunna1980@gmail.com>
2020-09-12 17:10:49 +0100

    Updated Nav bar

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@
 // Menu slide in and out
 //----------------------
 
+const menu = document.querySelector('.contain');
+const list = document.querySelector('.list');
+
+const topBar = document.querySelector('.top');
+const midBar = document.querySelector('.mid');
+const botBar = document.querySelector('.bot');
+
+menu.addEventListener('click', () => {
+	topBar.classList.toggle('changeTop');
+	midBar.classList.toggle('changeMid');
+	botBar.classList.toggle('changeBot');
+
+list.classList.toggle('hidden')
+});
+
+
