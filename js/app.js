a73a31a3a9ac6390b7c7c2ddfb512762bb8512a8
Claire Tregunna <c.tregunna1980@gmail.com>
2020-09-16 12:10:29 +0100

    JS to append note - not currently working

--- js/app.js
+++ js/app.js
@@ -57,6 +57,53 @@ modalClose.addEventListener('click', e => {
 //append it to the note container
 //eeek!!
 
+const submit = document.querySelector('.note-btn');
+const noteTitle = document.getElementById('note-title');
+const noteContent = document.getElementById('note-content');
+const noteContainer = document.querySelector('note-container');
+
+submit.addEventListener('click', (e) => {{
+        let note = document.createElement("DIV");
+        let noteContain = document.createElement("DIV");
+        let noteTit = document.createElement("H2");
+        let noteCon = document.createElement("P");
+        note.classList.add('note');
+        noteContain.classList.add('note-content');
+        noteTit.textContent = noteTitle.value;
+        noteCon.textContent = noteContent.value;
+        noteContain.appendChild(noteTit);
+        noteContain.appendChild(noteCon);
+        note.appendChild(noteContain);
+        noteContainer.appendChild(note);
+    }
+})
+
+
+///////From chatapp, use this to get you started//////
+
+// const messageInput = document.querySelector('input.addMessage');
+// const submitButton = document.querySelector('button.submit');
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', (e) => { //hit enter to submit form. Submit only works on form element
+//     e.preventDefault();
+//     if(messageInput.value === "") { //if you hit enter or send button it will submit form
+//         alert("Don't forget your message");
+//     } else {
+//     let message = document.createElement("DIV");  //create a div element
+//     message.classList.add('message'); // add class message to above div
+//     let text = document.createElement('P'); //create a p element
+//     let span = document.createElement('SPAN'); //create a span element
+//     text.textContent = messageInput.value; //add the vlue of the input to the p tag
+//     span.textContent = `from ${randomName(nameArray)}`; //add a random pres name to the span. Function randomName above creates this
+//     text.appendChild(span); //append the span to the p element
+//     message.appendChild(text); //append the p element to the div
+//     messageInput.value = ''; // return the input to a empty state
+//     messageBlock.appendChild(message); //append the message div to the message-display area
+//     }
+//   });
+
+
 //----------------------
 // Create random color for dot on the page notes
 //----------------------
